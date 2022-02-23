823238cfcf461083f968f36cd01cddf268c91ed3
liguanlin <517445163@qq.com>
2022-02-23 12:21:27 +0800

    simplify test.py

--- test.py
+++ test.py
@@ -49,19 +49,21 @@ def test():
     lr_decay_iters = 50
     continue_train = False
     results_dir = './results/'
-
+    name = 'retinexltifpm_allihd'
     if not os.path.exists(results_dir):
         os.mkdir(results_dir)
+
+    results_save_dir = os.path.join(results_dir, name)
+    if not os.path.exists(results_save_dir):
+        os.mkdir(results_save_dir)
 
     test_set = IhdDataset(dataset_root='../IntrinsicHarmony/datasets/HVIDIT/', dataset_name='HVIDIT', isTrain=False, crop_size=256)
     sampler = DistributedSampler(test_set) if NUM_GPUS > 1 else None
     dataset = DataLoader(dataset=test_set, batch_size=batch_size, shuffle=False, sampler=sampler)
-    dataset_size = len(dataset)    # get the number of images in the dataset.
 
     cur_device = torch.cuda.current_device()
     device = DEVICE
     print('cur_device=', cur_device)    
-    save_dir = os.path.join('./checkpoints', 'retinexltifpm_allihd')  # save all the checkpoints to save_dir
     model = retinexltifpmModel(device, False)
 
     model.setup('linear', epoch_count, niter, niter_decay, lr_decay_iters, continue_train)               # regular setup: load and print networks; create schedulers
@@ -73,10 +75,6 @@ def test():
     model.eval()
     ismaster = du.is_master_proc(NUM_GPUS)
 
-    fmse_score_list = []
-    mse_scores = 0
-    fmse_scores = 0
-    num_image = 0
     for i, data in enumerate(dataset):
         model.set_input(data)  # unpack data from data loader
         model.test()           # run inference
@@ -85,50 +83,25 @@ def test():
         img_path = model.get_image_paths()     # get image paths # Added by Mia
         if i % 5 == 0 and ismaster:  # save images to an HTML file
             print('processing (%04d)-th image... %s' % (i, img_path))
-        visuals_ones = OrderedDict()
-        harmonized = None
-        real = None
+
         #print(len(img_path))
 
         short_path = ntpath.basename(img_path[0]) #v0000_1_0.jpg
         name = os.path.splitext(short_path)[0] #v0000_1_0
 
-        im = util.tensor2im(model.harmonized) #将tensor转换为图像
-        image_name = '%s_%s.jpg' % (name, 'harmonized')
-        save_path = os.path.join(results_dir, image_name)
-        util.save_image(im, save_path, aspect_ratio=1.0)
+        visuals = OrderedDict()
+        visuals['harmonized'] = model.harmonized
+        visuals['real'] = model.real
+        visuals['mask'] = model.mask
+        visuals['illumination'] = model.illumination
+        visuals['reflectance'] = model.reflectance
 
-        im = util.tensor2im(model.real) #将tensor转换为图像
-        image_name = '%s_%s.jpg' % (name, 'real')
-        save_path = os.path.join(results_dir, image_name)
-        util.save_image(im, save_path, aspect_ratio=1.0)
+        for label, im_data in visuals.items():
+            im = util.tensor2im(im_data) #将tensor转换为图像
+            image_name = '%s_%s.jpg' % (name, label)
+            save_path = os.path.join(results_save_dir, image_name)
+            util.save_image(im, save_path, aspect_ratio=1.0)
 
-        im = util.tensor2im(model.mask) #将tensor转换为图像
-        image_name = '%s_%s.jpg' % (name, 'mask')
-        save_path = os.path.join(results_dir, image_name)
-        util.save_image(im, save_path, aspect_ratio=1.0)
-
-        im = util.tensor2im(model.illumination) #将tensor转换为图像
-        image_name = '%s_%s.jpg' % (name, 'illumination')
-        save_path = os.path.join(results_dir, image_name)
-        util.save_image(im, save_path, aspect_ratio=1.0)
-
-        im = util.tensor2im(model.reflectance) #将tensor转换为图像
-        image_name = '%s_%s.jpg' % (name, 'reflectance')
-        save_path = os.path.join(results_dir, image_name)
-        util.save_image(im, save_path, aspect_ratio=1.0)
-
-
-
-"""
-        for j in range(len(img_path)):
-            img_path_one = []
-            for label, im_data in visuals.items():
-                visuals_ones[label] = im_data[j:j+1, :, :, :]
-            img_path_one.append(img_path[j])
-            num_image += 1
-            visuals_ones.clear()
-"""        
 
 if __name__ == '__main__':
     test()
